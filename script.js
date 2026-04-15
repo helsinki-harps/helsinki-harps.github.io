b030ced88d51bf42741889cc86a3140271eaf108
Martti Levo <94462116+Walkerlad@users.noreply.github.com>
2026-04-15 13:19:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,9 +31,7 @@ if ("IntersectionObserver" in window && revealItems.length > 0) {
         observer.unobserve(entry.target);
       }
     });
-  }, {
-    threshold: 0.15
-  });
+  }, { threshold: 0.15 });
 
   revealItems.forEach((item) => observer.observe(item));
 } else {
